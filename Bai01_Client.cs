c327977aa09e656d6a5ee622316e43960ed7cdc2
kwebl <kwebl@WUANGTIEN>
2024-10-14 08:07:06 +0700

    Add project files.

---
+++ Bai01_Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab03_23521572_LeQuangTien
+{
+    public partial class Bai01_Client : Form
+    {
+        public Bai01_Client()
+        {
+            InitializeComponent();
+        }
+
+        private void Bai01_Client_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
